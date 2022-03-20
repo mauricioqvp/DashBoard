258d44a00a24a552c1814756fc00a8c49ea9fe80
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2022-03-20 20:36:14 -0300

    Personailizando scroll bar e aside fixo

--- minha-carteira/src/pages/List/index.tsx
+++ minha-carteira/src/pages/List/index.tsx
@@ -1,22 +1,109 @@
 import React from 'react';
 import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
+import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 
-import { Container } from './styles';
+import { Container, Content } from './styles';
 
 const List: React.FC = () => {
 
     const options = [
-        { value: 'Mauricio', label: 'Mauricio'},
-        { value: 'Caqui', label: 'Caqui'},
-        { value: 'Pera', label: 'Pera'}
+        { value: 'Mauricio', label: 'Mauricio' },
+        { value: 'Caqui', label: 'Caqui' },
+        { value: 'Pera', label: 'Pera' }
     ];
 
     return (
         <Container>
             <ContentHeader title="Saídas" lineColor="#E44C4E">
-                <SelectInput options={options}/>
+                <SelectInput options={options} />
             </ContentHeader>
+
+            <Content>
+                <HistoryFinanceCard
+                    tagColor="#E44C4E"
+                    title="Conta de luz"
+                    subtitle="27/07/2022"
+                    amount="R$ 130,00"
+                />
+
+                <HistoryFinanceCard
+                    tagColor="#E44C4E"
+                    title="Conta de luz"
+                    subtitle="27/07/2022"
+                    amount="R$ 130,00"
+                />
+
+                <HistoryFinanceCard
+                    tagColor="#E44C4E"
+                    title="Conta de luz"
+                    subtitle="27/07/2022"
+                    amount="R$ 130,00"
+                />
+
+                <HistoryFinanceCard
+                    tagColor="#E44C4E"
+                    title="Conta de luz"
+                    subtitle="27/07/2022"
+                    amount="R$ 130,00"
+                />
+
+                <HistoryFinanceCard
+                    tagColor="#E44C4E"
+                    title="Conta de luz"
+                    subtitle="27/07/2022"
+                    amount="R$ 130,00"
+                />
+
+                <HistoryFinanceCard
+                    tagColor="#E44C4E"
+                    title="Conta de luz"
+                    subtitle="27/07/2022"
+                    amount="R$ 130,00"
+                />
+
+                <HistoryFinanceCard
+                    tagColor="#E44C4E"
+                    title="Conta de luz"
+                    subtitle="27/07/2022"
+                    amount="R$ 130,00"
+                />
+
+                <HistoryFinanceCard
+                    tagColor="#E44C4E"
+                    title="Conta de luz"
+                    subtitle="27/07/2022"
+                    amount="R$ 130,00"
+                />
+
+                <HistoryFinanceCard
+                    tagColor="#E44C4E"
+                    title="Conta de luz"
+                    subtitle="27/07/2022"
+                    amount="R$ 130,00"
+                />
+
+                <HistoryFinanceCard
+                    tagColor="#E44C4E"
+                    title="Conta de luz"
+                    subtitle="27/07/2022"
+                    amount="R$ 130,00"
+                />
+
+                <HistoryFinanceCard
+                    tagColor="#E44C4E"
+                    title="Conta de luz"
+                    subtitle="27/07/2022"
+                    amount="R$ 130,00"
+                />
+
+                <HistoryFinanceCard
+                    tagColor="#E44C4E"
+                    title="Conta de luz"
+                    subtitle="27/07/2022"
+                    amount="R$ 130,00"
+                />
+            </Content>
         </Container>
     );
 }
